8f1d4c20d75442e6490518361e42c25105f70e42
hongdakyung <bongbk72@naver.com>
2023-03-09 10:17:11 +0900

    test

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -5,7 +5,8 @@ const Footer = () => {
   const { version } = packageJson;
 
   return (
-    <div className="bg-[#FFF5F5] flex flex-col justify-start text-mono100">
+    <div className="w-[1260px] bg-[#FFF5F5] flex flex-col justify-start text-mono100">
+      {/* <div className="bg-[#FFF5F5] flex flex-col justify-start text-mono100"> */}
       <ul className="border-b border-white pt-6 w-4/6 mx-auto">
         <li className="mb-4">
           <span className="mr-7 w-[52px] h-[18px] text-xs">고객센터</span>
